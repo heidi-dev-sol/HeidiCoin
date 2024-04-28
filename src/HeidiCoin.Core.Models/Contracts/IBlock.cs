3051c076a73a5b1acd0562e4a7ae085963f0d9c2
Heidi Developer (Ollie) <heidi.dev.sol@gmail.com>
2024-04-28 07:16:23 +0100

    Initial commit to include some basic POCO's

---
+++ src/HeidiCoin.Core.Models/Contracts/IBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeidiCoin.Core.Models.Contracts
+{
+    public interface IBlock : IBlock<string> { }
+
+    public interface IBlock<TData>
+    {
+        public long Index { get; set; }
+        public string PreviousHash { get; set; }
+        public DateTime Timestamp { get; set; }
+        public TData Data { get; set; }
+        public string Hash { get; set; }
+        public long Nonce { get; set; }
+    }
+}
---
+++ src/HeidiCoin.Core.Models/Implementations/Block.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeidiCoin.Core.Models.Implementations
+{
+    using HeidiCoin.Core.Models.Contracts;
+
+    //The main block class that represents a single block.
+    public class Block<TData> : IBlock<TData>
+    {
+        public long Index { get; set; }
+        public string PreviousHash { get; set; }
+        public DateTime Timestamp { get; set; }
+        public TData Data { get; set; }
+        public string Hash { get; set; }
+        public long Nonce { get; set; }
+    }
+
+    public class Block : Block<string>, IBlock { }
+}
